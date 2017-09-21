13eb8350d50b986997964be65b56d5ccff0c7cd0
8ayac <8ayac.y@gmail.com>
2017-09-21 11:06:16 +0900

    [add] 00_install: それぞれの設定ファイルを単体で導入するシェルスクリプト群

---
+++ 00_install/vim-install.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+ln -sf -b ${dotfiles}/vimrc/_vimrc $HOME/.vimrc
+ln -sf -b ${dotfiles}/vimrc/userautoload/ $HOME/.vim/userautoload
---
+++ 00_install/zsh-install.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+ln -sf -b ${dotfiles}/zshrc/_zshrc $HOME/.zshrc
