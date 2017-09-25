c45260edf8ce45e9c5d52ea533622956371367bc
8ayac <8ayac.y@gmail.com>
2017-09-25 16:57:31 +0900

    [add] install.sh: zsh-syntax-highlightingを追加した。

--- install.sh
+++ install.sh
@@ -6,5 +6,8 @@ installsh_dir=${dotfiles}/00_install
 # zsh
 sh ${installsh_dir}/zsh-install.sh
 
+# zsh-syntax-highlighting
+sh ${installsh_dir}/zsh-syntax-highlighting-install.sh
+
 # vim
 sh ${installsh_dir}/vim-install.sh
