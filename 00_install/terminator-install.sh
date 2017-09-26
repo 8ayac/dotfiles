982ae1538363894a58b3fab246389ed16c6445e7
8ayac <8ayac.y@gmail.com>
2017-09-26 15:53:42 +0900

    [update] terminator-install.sh: terminator-installが導入されていなかった場合、インストールするようにした。

--- 00_install/terminator-install.sh
+++ 00_install/terminator-install.sh
@@ -2,5 +2,8 @@
 
 . $HOME/dotfiles/00_install/0_func.func
 
+if [ ! -e $HOME/.config/terminator/config ]; then
+    sudo apt -y install terminator
+fi
 backup $HOME/.config/terminator/config terminator
 ln -sf ${dotfiles}/terminator/config $HOME/.config/terminator/config
