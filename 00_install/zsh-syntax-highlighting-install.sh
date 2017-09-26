6919a6349571e9e4eb724a84084d5c3622a3c2ec
8ayac <8ayac.y@gmail.com>
2017-09-26 15:39:38 +0900

    [update] zsh-syntax-highlighting-install.sh: zsh-syntax-highlighting-installが導入されていなかった場合、インストールするようにした。

--- 00_install/zsh-syntax-highlighting-install.sh
+++ 00_install/zsh-syntax-highlighting-install.sh
@@ -3,4 +3,7 @@
 . $HOME/dotfiles/00_install/0_func.func
 
 backup $HOME/zsh-syntax-highlighting/highlighters zsh-syntax-highlighting
+if [ ! -e $HOME/zsh-syntax-highlighting/ ]; then
+	git clone https://github.com/zsh-syntax-highlighting.git $HOME/zsh-syntax-highlighting
+fi
 ln -sf ${dotfiles}/zsh-syntax-highlighting/highlighters $HOME/zsh-syntax-highlighting/highlighters
