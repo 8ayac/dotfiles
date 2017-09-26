921f64b65a29eaee265a184ca4c11757a4ea65ae
8ayac <8ayac.y@gmail.com>
2017-09-27 00:01:30 +0900

    [update] zsh-syntax-highlighting-install.sh: ダイナミックリンクをディレクトリではなく、ファイルごとに貼るように変更した。

--- 00_install/zsh-syntax-highlighting-install.sh
+++ 00_install/zsh-syntax-highlighting-install.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
 
 . $HOME/dotfiles/00_install/0_func.func
+dir=${dofiles}/zsh-syntax-highlighting/highlighters
 
 if [ ! -e $HOME/zsh-syntax-highlighting/ ]; then
 	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/zsh-syntax-highlighting
 fi
 backup $HOME/zsh-syntax-highlighting/highlighters zsh-syntax-highlighting
-ln -sf ${dotfiles}/zsh-syntax-highlighting/highlighters $HOME/zsh-syntax-highlighting/highlighters
+
+ln -sf ${dir}/brackets-highlighter.zsh $HOME/zsh-syntax-highlighting/highlighters/brackets/brackets-highlighter.zsh
+ln -sf ${dir}/cursor-highlighter.zsh $HOME/zsh-syntax-highlighting/highlighters/cursor/cursor-highlighter.zsh
+ln -sf ${dir}/line-highlighters.zsh $HOME/zsh-syntax-highlighting/highlighters/line/line-highlighter.zsh
+ln -sf ${dir}/main-highlighters.zsh $HOME/zsh-syntax-highlighting/highlighters/main/main-highlighter.zsh
+ln -sf ${dir}/pattern-highlighters.zsh $HOME/zsh-syntax-highlighting/highlighters/pattern/pattern-highlighter.zsh
+ln -sf ${dir}/root-highlighters.zsh $HOME/zsh-syntax-highlighting/highlighters/root/root-highlighter.zsh
