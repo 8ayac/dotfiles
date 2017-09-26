0a306744776db6f95142fd2309e3471babef8afd
8ayac <8ayac.y@gmail.com>
2017-09-26 15:19:22 +0900

    [bugfix] vim-install.sh: バックアップ元のファイルの指定がディレクトリになっていたので、適当に修正した。

--- 00_install/vim-install.sh
+++ 00_install/vim-install.sh
@@ -2,6 +2,6 @@
 
 . $HOME/dotfiles/00_install/0_func.func
 
-backup $HOME/.vimrc/ _vimrc
+backup $HOME/.vimrc _vimrc
 ln -sf ${dotfiles}/vimrc/_vimrc $HOME/.vimrc
 ln -sf ${dotfiles}/vimrc/userautoload/ $HOME/.vim/userautoload
