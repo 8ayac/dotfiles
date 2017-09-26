249d14aa3d54f64e0a7eadc8d7194f2986d59b9c
8ayac <8ayac.y@gmail.com>
2017-09-26 16:11:16 +0900

    [modify] zsh-syntax-highlighting-install.sh: clone元のリポジトリ名が間違っていたので修正した。

--- 00_install/zsh-syntax-highlighting-install.sh
+++ 00_install/zsh-syntax-highlighting-install.sh
@@ -3,7 +3,7 @@
 . $HOME/dotfiles/00_install/0_func.func
 
 if [ ! -e $HOME/zsh-syntax-highlighting/ ]; then
-	git clone https://github.com/zsh-syntax-highlighting.git $HOME/zsh-syntax-highlighting
+	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/zsh-syntax-highlighting
 fi
 backup $HOME/zsh-syntax-highlighting/highlighters zsh-syntax-highlighting
 ln -sf ${dotfiles}/zsh-syntax-highlighting/highlighters $HOME/zsh-syntax-highlighting/highlighters
