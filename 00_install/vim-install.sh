5a5bbe3e0ddb1a0d31761c187b9b0cb0c88f7273
8ayac <8ayac.y@gmail.com>
2017-09-26 15:49:34 +0900

    [update] 00_install内のファイル(vim-install, zsh-install): vim, zshが導入されていなかった場合、インストールするようにした。

--- 00_install/vim-install.sh
+++ 00_install/vim-install.sh
@@ -2,6 +2,10 @@
 
 . $HOME/dotfiles/00_install/0_func.func
 
+if [ ! -e $HOME/.vimrc ]; then
+    sudo apt -y install vim
+    sudo apt -y install vim-gnome
+fi
 backup $HOME/.vimrc _vimrc
 ln -sf ${dotfiles}/vimrc/_vimrc $HOME/.vimrc
 ln -sf ${dotfiles}/vimrc/userautoload/ $HOME/.vim/userautoload
--- 00_install/zsh-install.sh
+++ 00_install/zsh-install.sh
@@ -2,5 +2,8 @@
 
 . $HOME/dotfiles/00_install/0_func.func
 
+if [ ! -e $HOME/.zshrc ]; then
+    sudo apt -y install zsh
+fi
 backup $HOME/.zshrc _zshrc
 ln -sf ${dotfiles}/zshrc/_zshrc $HOME/.zshrc
