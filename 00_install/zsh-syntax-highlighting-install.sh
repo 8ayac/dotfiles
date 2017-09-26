41768ff90499b7ed6b4c48db6bf1a26e86a34b03
8ayac <8ayac.y@gmail.com>
2017-09-26 15:43:10 +0900

    [update] zsh-syntax-highlighting-install.sh: zsh-syntax-highlighting-installが導入されていなかった場合、インストールするタイミングをバックアップを取る前に変更した。

--- 00_install/zsh-syntax-highlighting-install.sh
+++ 00_install/zsh-syntax-highlighting-install.sh
@@ -2,8 +2,8 @@
 
 . $HOME/dotfiles/00_install/0_func.func
 
-backup $HOME/zsh-syntax-highlighting/highlighters zsh-syntax-highlighting
 if [ ! -e $HOME/zsh-syntax-highlighting/ ]; then
 	git clone https://github.com/zsh-syntax-highlighting.git $HOME/zsh-syntax-highlighting
 fi
+backup $HOME/zsh-syntax-highlighting/highlighters zsh-syntax-highlighting
 ln -sf ${dotfiles}/zsh-syntax-highlighting/highlighters $HOME/zsh-syntax-highlighting/highlighters
