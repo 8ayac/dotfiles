79b8536d6efe1e53f37a5da2c658a204ba2cce4f
8ayac <8ayac.y@gmail.com>
2017-09-26 13:20:03 +0900

    [bugfix] 00_install/内のファイル: 絶対パスが間違っていたので、修正した。

--- 00_install/terminator-install.sh
+++ 00_install/terminator-install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-dotfiles=$(cd $(dirname $0) && pwd)
-. ${dotfiles}/0_func.func
+dotfiles=$(cd $(dirname $0) && pwd)/..
+. ${dotfiles}/00_install/0_func.func
 
-backup $HOME/dotfiles/terminator terminator
+backup $HOME/.config/terminator/config terminator
 ln -sf ${dotfiles}/terminator/config $HOME/.config/terminator/config
--- 00_install/zsh-install.sh
+++ 00_install/zsh-install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-dotfiles=$(cd $(dirname $0) && pwd)
-. ${dotfiles}/0_func.func
+dotfiles=$(cd $(dirname $0) && pwd)/..
+. ${dotfiles}/00_install/0_func.func
 
 backup $HOME/.zshrc .zshrc
 ln -sf ${dotfiles}/zshrc/_zshrc $HOME/.zshrc
