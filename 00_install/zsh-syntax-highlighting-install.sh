cc4edf2c8b7dfaca29cb53080b56484585f33dcd
8ayac <8ayac.y@gmail.com>
2017-09-27 09:03:36 +0900

    [bugfix] zsh-syntax-highlighting-install.sh: lnではうまく動作しないため、cp修正した。

--- 00_install/zsh-syntax-highlighting-install.sh
+++ 00_install/zsh-syntax-highlighting-install.sh
@@ -8,9 +8,9 @@ if [ ! -e $HOME/zsh-syntax-highlighting/ ]; then
 fi
 backup $HOME/zsh-syntax-highlighting/highlighters zsh-syntax-highlighting
 
-ln -sf ${dir}/brackets-highlighter.zsh $HOME/zsh-syntax-highlighting/highlighters/brackets/brackets-highlighter.zsh
-ln -sf ${dir}/cursor-highlighter.zsh $HOME/zsh-syntax-highlighting/highlighters/cursor/cursor-highlighter.zsh
-ln -sf ${dir}/line-highlighters.zsh $HOME/zsh-syntax-highlighting/highlighters/line/line-highlighter.zsh
-ln -sf ${dir}/main-highlighters.zsh $HOME/zsh-syntax-highlighting/highlighters/main/main-highlighter.zsh
-ln -sf ${dir}/pattern-highlighters.zsh $HOME/zsh-syntax-highlighting/highlighters/pattern/pattern-highlighter.zsh
-ln -sf ${dir}/root-highlighters.zsh $HOME/zsh-syntax-highlighting/highlighters/root/root-highlighter.zsh
+cp -rf ${dir}/brackets-highlighter.zsh $HOME/zsh-syntax-highlighting/highlighters/brackets/brackets-highlighter.zsh
+cp -rf ${dir}/cursor-highlighter.zsh $HOME/zsh-syntax-highlighting/highlighters/cursor/cursor-highlighter.zsh
+cp -rf ${dir}/line-highlighters.zsh $HOME/zsh-syntax-highlighting/highlighters/line/line-highlighter.zsh
+cp -rf ${dir}/main-highlighters.zsh $HOME/zsh-syntax-highlighting/highlighters/main/main-highlighter.zsh
+cp -rf ${dir}/pattern-highlighters.zsh $HOME/zsh-syntax-highlighting/highlighters/pattern/pattern-highlighter.zsh
+cp -rf ${dir}/root-highlighters.zsh $HOME/zsh-syntax-highlighting/highlighters/root/root-highlighter.zsh
