a649d29316a745a6c564f7be310f9348f47eb5c8
8ayac <8ayac.y@gmail.com>
2018-02-20 21:21:32 +0900

    [add] install.sh: nvimの項目を追加した。

--- install.sh
+++ install.sh
@@ -12,5 +12,8 @@ sh ${installsh_dir}/zsh-syntax-highlighting-install.sh
 # vim
 sh ${installsh_dir}/vim-install.sh
 
+# neovim
+sh ${installsh_dir}/neovim-install.sh
+
 # terminator
 sh ${installsh_dir}/terminator-install.sh
