af12a730383b8290adf8ad7d686cc35b96e8fcc4
8ayac <8ayac.y@gmail.com>
2018-04-23 13:03:41 +0900

    [add] GOPATHを追加した

--- zshrc/config/a2_export.zsh
+++ zshrc/config/a2_export.zsh
@@ -6,3 +6,7 @@ export LANG=ja_JP.UTF-8
 export TERM='xterm-256color'
 export PATH=$PATH:$HOME/tmp
 export PATH=$PATH:$HOME/mgtools
+
+# go
+export GOPATH=$HOME/go
+export PATH=$PATH:$GOROOT/bin:$GOPATH/bin
